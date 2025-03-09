78722adc04936de1c49089c482e7cdfc21042cfb
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2025-03-09 07:54:42 +0330

    [NEW]: Shrink the occupied memory if the size of the array is 1/3 of capacity

--- DataStructure/source/main.cpp
+++ DataStructure/source/main.cpp
@@ -5,6 +5,15 @@
 int main() {
 	SGT::String str = "Hello World";
 	SGT::String str2 = "Hello World";
+	SGT::Vector<int> vec;
+	for (int i = 0; i < 15; i++) {
+		vec.push_back(i);
+	}
+	vec.printData();
+	for (int i = 0; i < 13; i++) {
+		vec.pop_back();
+	}
+	vec.printData();
 
 	std::cout << str.isTheSame("Hello World") << std::endl;
 }
