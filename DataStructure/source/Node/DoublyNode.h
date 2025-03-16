be076796cf5088688c2a1afd7d1f87d8d07d54fb
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2025-03-16 20:56:45 +0330

    [CHANGE]: Change class to the struct

--- DataStructure/source/Node/DoublyNode.h
+++ DataStructure/source/Node/DoublyNode.h
@@ -1,1 +1,22 @@
 #pragma once
+#pragma once
+
+namespace SGT {
+
+	template <typename _Type>
+	struct DoublyNode {
+	
+		_Type data;
+		DoublyNode<_Type>* nextNode;
+		DoublyNode<_Type>* previousNode;
+
+	
+		DoublyNode()
+			:data(), nextNode(nullptr), previousNode(nullptr) {
+		}
+		DoublyNode(const _Type& data)
+			:data(data), nextNode(nullptr), previousNode(nullptr) {
+		}
+	};
+
+}
--- DataStructure/source/Node/SinglyNode.h
+++ DataStructure/source/Node/SinglyNode.h
@@ -3,15 +3,14 @@
 namespace SGT {
 
 	template <typename _Type>
-	class SinglyNode {
-		_Type m_data;
-		SinglyNode* m_nextNode;
+	struct SinglyNode {
+		_Type data;
+		SinglyNode<_Type>* nextNode;
 
-	public:
 		SinglyNode()
-			:m_data(nullptr),m_nextNode(nullptr){}
+			:data(),nextNode(nullptr){}
 		SinglyNode(const _Type& data)
-			:m_data(data), m_nextNode(nullptr) {
+			:data(data), nextNode(nullptr) {
 		}
 	};
 
