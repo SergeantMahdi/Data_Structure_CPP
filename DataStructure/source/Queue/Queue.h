867ae220df9a2a6750cea7c1e0ba85c1dfba783f
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2025-04-15 12:31:52 +0330

    [NEW]: Add Queue with push and print data functions

---
+++ DataStructure/source/Queue/Queue.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "../Node/SinglyNode.h"
+namespace SGT{
+
+template <typename _Type>
+class Queue {
+private:
+	using Node = SGT::SinglyNode<_Type>;
+
+private:
+	Node* m_head;
+	Node* m_tail;
+	size_t m_size;
+
+public:
+
+	Queue()
+		:m_head(nullptr),m_size(0){}
+
+	~Queue() {
+		Node* head = m_head;
+		Node* temp = m_head;
+
+		while (temp) {
+			temp = temp->nextNode;
+			delete head;
+			head = temp;
+			
+		}
+	}
+	void push(const _Type& data) {
+
+		Node* newNode = new Node(data);
+
+		if (!m_head) {
+			m_head = newNode;
+			m_tail = newNode;
+			m_size++;
+		}
+
+		m_tail->nextNode = newNode;
+		m_tail = newNode;
+		m_size++;
+
+	}
+
+	void printData() {
+		Node* temp = m_head;
+
+		while (temp) {
+			std::cout << "[" << &temp << "]: " << temp->data << std::endl;
+			temp = temp->nextNode;
+		}
+	}
+
+};
+
+}
